77b10434e1fafcc1d169f0c10c894c2fdeee3dd8
Danieltacorich <dlopezgines1@knights.ucf.edu>
2022-11-06 00:39:57 -0400

    BigBoy Commit

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -4,17 +4,80 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    //Health & Invincibility
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+     bool isInvincible;
+    float invincibleTimer;
+
+    //Rigidbody and Movement
+    public float speed = 3.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    
+    //Animation
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        // Tester currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position; 
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        //Invicibility
+         if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+    //Health & Invincibility 
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
